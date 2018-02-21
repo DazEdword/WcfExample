1b58029837aa535f0196ed75e2d23c3bdba71b4b
Ed Garabito <eduardo@gottabegarabi.com>
2018-02-21 17:08:37 +0000

    Removed comments.

--- WcfTestApp/WcfTestApp/CalcService.svc.cs
+++ WcfTestApp/WcfTestApp/CalcService.svc.cs
@@ -8,8 +8,6 @@ using System.Threading.Tasks;
 
 namespace WcfTestApp
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalcService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select CalcService.svc or CalcService.svc.cs at the Solution Explorer and start debugging.
     public class CalcService : ICalcService
     {
         public int Add(int a, int b)
