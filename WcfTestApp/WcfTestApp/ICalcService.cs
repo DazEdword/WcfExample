7dcf728d302a21ea5be85257a3ca4288fcbfe062
Ed Garabito <eduardo@gottabegarabi.com>
2018-02-21 16:00:20 +0000

    Adding example CalcService.

---
+++ WcfTestApp/WcfTestApp/ICalcService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfTestApp
+{
+    [ServiceContract]
+    public interface ICalcService
+    {
+        [OperationContract]
+        int Add(int a, int b);
+        int Substract(int a, int b);
+    }
+}
