a6e320b39ef7449b7bb915d1b6cf2b81985b74ff
Ed Garabito <eduardo@gottabegarabi.com>
2018-02-24 22:20:10 +0000

    Refactored console app to be able to mock input, added tests.

--- WcfTestApp/ConsoleTestBinding/Program.cs
+++ WcfTestApp/ConsoleTestBinding/Program.cs
@@ -2,23 +2,30 @@
 
 namespace ConsoleTestBinding
 {
-    class Program
+    public class CalcConsoleClient
     {
-        static void Main(string[] args)
+        public static string[] availableOperations = { "add", "subtract" };
+        public static IGetInput Input = new Input();
+
+        public static void Main(string[] args)
         {
             using (var client = new CalcServiceReference.CalcServiceClient())
             {
-                Console.WriteLine("Let's add some numbers.");
-                Console.WriteLine("Please type your first digit and press enter.");
+                Console.WriteLine("Welcome to the WCF calculator. Please make sure to type your input and press enter when prompted.");
+                Console.WriteLine(String.Format("Let's do some simple math. The available operations are: {0}", String.Join(", ", availableOperations)));
+                Console.WriteLine("What operation would you like the app to do?");
 
-                int a = GetUserInput();
+                string operation = GetUserOperationInput();
+                Console.WriteLine("First digit?");
 
-                Console.WriteLine("Please type your second digit and press enter.");
-                int b = GetUserInput();
+                int a = GetUserNumericalInput();
+
+                Console.WriteLine("Second digit?");
+                int b = GetUserNumericalInput();
 
                 Console.WriteLine("Sending request to server...");
-                var req = new CalcServiceReference.Request()
-                {
+                var req = new CalcServiceReference.Request() {
+                    operation = operation,
                     a = a,
                     b = b
                 };
@@ -37,17 +44,52 @@ namespace ConsoleTestBinding
                 Console.ReadKey();
             }
         }
-
-        static int GetUserInput()
+       
+        //TODO limits of int32 apply, ranging -2,147,483,648 to 2,147,483,647
+        static int GetUserNumericalInput()
         {
             int value;
 
-            while (!int.TryParse(Console.ReadLine(), out value))
+            while (!int.TryParse(Input.GetUserInput(), out value))
             {
                 Console.WriteLine("Please Enter a valid numerical value!");
             }
 
             return value;
         }
+
+        static string GetUserOperationInput() {
+            string userInput = null;
+
+            bool valid = false;
+
+            while (!valid) {
+                userInput = Input.GetUserInput();
+
+                switch (userInput) {
+                    case "add":
+                        valid = true;
+                        break;
+                    case "subtract":
+                        valid = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter a valid operation!");
+                        break;
+                }               
+            }
+
+            return userInput;
+        }
+    }
+
+    public class Input : IGetInput{
+        public string GetUserInput() {
+            return Console.ReadLine();
+        }
+    }
+
+    public interface IGetInput {
+        string GetUserInput();
     }
 }
---
+++ WcfTestApp/WcfTests/ConsoleApplication/ConsoleAppUserInput.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleTestBinding;
+using NUnit.Framework;
+using NSubstitute;
+using RunHelper = WcfTests.UnitTestUtilities.RunHelper;
+using Target = ConsoleTestBinding.CalcConsoleClient;
+
+namespace WcfTests.ConsoleApplication {
+
+    [TestFixture]
+    public class ConsoleAppUserInput {
+
+        [SetUp]
+        public void SetUp() {
+            
+        }
+
+        [Test]
+        public void GetUserNumericalInputReturnsIntegerWhenInputIsInteger() {
+            //Arrange
+            IGetInput InputMock = Substitute.For<IGetInput>();
+            InputMock.GetUserInput().Returns(x => "5");
+            int expected = 5;
+
+            Target.Input = InputMock;
+
+            //Act
+            var result = RunHelper.RunStaticMethod(typeof(CalcConsoleClient), "GetUserNumericalInput", null);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetUserNumericalInputDoesNotReturnIfInputIsOtherThanInteger() {
+            //Arrange
+            IGetInput InputMock = Substitute.For<IGetInput>();
+            InputMock.GetUserInput().Returns(x => "5");
+            int expected = 5;
+
+            Target.Input = InputMock;
+
+            //Act
+            var result = RunHelper.RunStaticMethod(typeof(CalcConsoleClient), "GetUserNumericalInput", null);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
